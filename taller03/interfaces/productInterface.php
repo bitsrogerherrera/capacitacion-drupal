2f08eb4bae1263a61a4e955c4b69f3f4d0f2d014
Alberto Rodriguez <alberto.rodriguez@bitsamericas.com>
2021-03-26 09:50:54 -0500

    introduccion a php

---
+++ taller03/interfaces/productInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+interface producto
+{
+    public function getTitle();
+    public function setTitle($title);
+    public function getPrice();
+    public function setPrice($price);
+    public function description();
+}
