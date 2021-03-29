d7137582e8a98ce5bb65596ddb0d5def0cd469e2
Julio Rico <Julio.Rico@bitsamericas.com>
2021-03-29 13:31:36 -0500

    Taller 03 - Introduccion PHP

---
+++ bitsjuliorico/php/taller03/interfaceProduct.php
@@ -0,0 +1,9 @@
+<?php
+namespace taller03\ProductInterface;
+interface ProductInterface{
+    public function getName();
+    public function setName($name);
+    public function getPrice();
+    public function setPrice($price);
+
+} 
